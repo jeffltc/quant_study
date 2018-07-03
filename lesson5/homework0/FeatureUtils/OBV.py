f77aff1adf8f48857dc441e69f3196c5e526c7e0
Jeff Zhang <zhangjue_88@126.com>
2018-07-04 06:43:05 +0800

    2018-07-04 06:43:05

---
+++ lesson5/homework0/FeatureUtils/OBV.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  4 06:23:28 2018
+
+@author: newchama
+"""
+
+
+#The On Balance Volume (OBV) line is simply a running total of positive and negative volume.
+# A period's volume is positive when the close is above the prior close. 
+# A period's volume is negative when the close is below the prior close.
+#
+#If the closing price is above the prior close price then: 
+#Current OBV = Previous OBV + Current Volume
+#
+#If the closing price is below the prior close price then: 
+#Current OBV = Previous OBV  -  Current Volume
+#
+#If the closing prices equals the prior close price then:
+#Current OBV = Previous OBV (no change)
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import tushare as ts
+
+
+
+def OBV(data, ndays): 
+    OBV = [data['volume'][0]]
+    
+    for i in range(1,len(data)):
+        if data['close'].iloc[i] < data['close'].iloc[i-1]:
+            current_OBV = OBV[i-1] - data['volume'].iloc[i]
+        elif data['close'].iloc[i] == data['close'].iloc[i-1]:
+            current_OBV = OBV[i-1]
+        elif data['close'].iloc[i] > data['close'].iloc[i-1]:
+            current_OBV = OBV[i-1] + data['volume'].iloc[i]
+        OBV.append(current_OBV)
+    
+    OBV = pd.Series(OBV, name = 'On Balance Volumn') 
+    data = data.join(OBV) 
+    return data
+
+
+
+# Retrieve  data from tushare:
+data = ts.get_k_data('600000',start='2010-01-01', end='2016-01-01')
+data = pd.DataFrame(data)
+data = OBV(data,1)
