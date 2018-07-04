29416e4431a298fc96b85aab731606acf01aabdd
Jeff Zhang <zhangjue_88@126.com>
2018-07-05 06:47:46 +0800

    2018-07-05 06:47:45

---
+++ lesson5/homework0/FeatureUtils/SO.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  5 06:28:23 2018
+
+@author: newchama
+"""
+
+#%K = (Current Close - Lowest Low)/(Highest High - Lowest Low) * 100
+#%D = 3-day SMA of %K
+#
+#Lowest Low = lowest low for the look-back period
+#Highest High = highest high for the look-back period
+#%K is multiplied by 100 to move the decimal point two places
+
