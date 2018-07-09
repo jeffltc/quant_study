3c8ba142eb904dcf2e4bacab886fe51676e55a36
Jeff Zhang <zhangjue_88@126.com>
2018-07-09 08:17:15 +0800

    2018-07-09 08:17:15

--- lesson5/homework0/forecast.py
+++ lesson5/homework0/forecast.py
@@ -10,12 +10,13 @@ import numpy as np
 import pandas as pd
 import sklearn
 
-from pandas.io.data import DataReader
+
+import tushare as ts
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.lda import LDA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.metrics import confusion_matrix
-from sklearn.qda import QDA
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis as QDA
 from sklearn.svm import LinearSVC, SVC
 
 
@@ -30,20 +31,16 @@ def create_lagged_series(symbol, start_date, end_date, lags=5):
     """
 
     # Obtain stock information from Yahoo Finance
-    ts = DataReader(
-    	symbol, "yahoo", 
-    	start_date-datetime.timedelta(days=365), 
-    	end_date
-    )
+    data = ts.get_k_data(code = symbol, index=True,start = start_date, end = end_date)
 
     # Create the new lagged DataFrame
-    tslag = pd.DataFrame(index=ts.index)
-    tslag["Today"] = ts["Adj Close"]
-    tslag["Volume"] = ts["Volume"]
+    tslag = pd.DataFrame(index=data.date)
+    tslag["Today"] = data["close"]
+    tslag["Volume"] = data["volume"]
 
     # Create the shifted lag series of prior trading period close values
     for i in range(0, lags):
-        tslag["Lag%s" % str(i+1)] = ts["Adj Close"].shift(i+1)
+        tslag["Lag%s" % str(i+1)] = data["close"].shift(i+1)
 
     # Create the returns DataFrame
     tsret = pd.DataFrame(index=tslag.index)
@@ -71,8 +68,8 @@ def create_lagged_series(symbol, start_date, end_date, lags=5):
 if __name__ == "__main__":
     # Create a lagged series of the S&P500 US stock market index
     snpret = create_lagged_series(
-    	"^GSPC", datetime.datetime(2001,1,10), 
-    	datetime.datetime(2005,12,31), lags=5
+    	'399300', '2001-01-10', 
+    	'2005-12-31', lags=5
     )
 
     # Use the prior two days of returns as predictor 
@@ -81,7 +78,9 @@ if __name__ == "__main__":
     y = snpret["Direction"]
 
     # The test data is split into two parts: Before and after 1st Jan 2005.
-    start_test = datetime.datetime(2005,1,1)
+#    start_test = datetime.datetime(2005,1,1)
+    
+    start_test = '2005-12-31'
 
     # Create training and test sets
     X_train = X[X.index < start_test]
@@ -91,6 +90,23 @@ if __name__ == "__main__":
    
     # Create the (parametrised) models
     print("Hit Rates/Confusion Matrices:\n")
+#    models = [("LR", LogisticRegression()), 
+#              ("LDA", LDA()), 
+#              ("QDA", QDA()),
+#              ("LSVC", LinearSVC()),
+#              ("RSVM", SVC(
+#              	C=1000000.0, cache_size=200, class_weight=None,
+#                coef0=0.0, degree=3, gamma=0.0001, kernel='rbf',
+#                max_iter=-1, probability=False, random_state=None,
+#                shrinking=True, tol=0.001, verbose=False)
+#              ),
+#              ("RF", RandomForestClassifier(
+#              	n_estimators=1000, criterion='gini', 
+#                max_depth=None, min_samples_split=2, 
+#                min_samples_leaf=1, max_features='auto', 
+#                bootstrap=True, oob_score=False, n_jobs=1, 
+#                random_state=None, verbose=0)
+#              )]
     models = [("LR", LogisticRegression()), 
               ("LDA", LDA()), 
               ("QDA", QDA()),
@@ -108,6 +124,7 @@ if __name__ == "__main__":
                 bootstrap=True, oob_score=False, n_jobs=1, 
                 random_state=None, verbose=0)
               )]
+
 
     # Iterate through the models
     for m in models:
